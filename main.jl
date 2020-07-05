64a2225fd66f2a5f1d014d5ad837b61ebefb90be
rannewan <rannewandter@posteo.net>
2020-07-05 20:13:57 +0100

    Add function prototypes

---
+++ main.jl
@@ -0,0 +1,24 @@
+
+
+function read_Species_From_Text_File(file_name)
+    return reaction_matrix
+end
+
+
+function transformation_matrix_ij(i,j,reaction_matrix)
+    return s_ij
+end
+
+function transformation_matrix_inverse_ij(i,j,reaction_matrix)
+    return s_ij
+end
+
+function assemble_transformation_matrix(reaction_matrix)
+    return transformation_matrix
+end
+
+function assemble_transformation_matrix_inverse(reaction_matrix)
+    return transformation_matrix_inverse
+end
+
+
