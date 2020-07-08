1db11c8ddd82d32e46866dc9e2ce7c168f4e91b1
rannewan <rannewandter@posteo.net>
2020-07-08 17:46:08 +0100

    Fix s_ij: indexing is now over species ancestor index

--- main.jl
+++ main.jl
@@ -8,37 +8,37 @@ end
 
 function transformation_matrix_ij(i,j,decay_rates)
     s_ij = ones(eltype(decay_rates[1]))
-    # Loop over generations
-    for l in 1: (i-j)
-        # Loop over species idnex of species ancestor
-        for m in j:(i-1)
-            # println(l,m)
-            # println(decay_rates[m], decay_rates[i])
-            # println(decay_rates[m] / (decay_rates[m] - decay_rates[i]))
-            s_ij *= decay_rates[m] / (decay_rates[m] - decay_rates[i])
-        end
+    # Loop over species ancestot index l for species i with (i-j) generations
+    for l in j: (i-1)
+        println(l)
+        println(decay_rates[l])
+        println( (decay_rates[l] - decay_rates[i]))
+        println(decay_rates[l] / (decay_rates[l] - decay_rates[i]))
+        s_ij *= decay_rates[l] / (decay_rates[l] - decay_rates[i])
     end
     return s_ij
 end
 
 
 
-function transformation_matrix_inverse_ij(i,j,decay_rates)
-     s_ij = ones(eltype(decay_rates[1]))
-     s_ij = decay_rates[j]/ (decay_rates[i] - decay_rates[j])
-    # Loop over generations
-    for l in 1: (i-j-1)
-        # Loop over species idnex of species ancestor
-        for m in j:(i-1)
-            println(l,m)
-            println(decay_rates[m], decay_rates[i])
-            println(decay_rates[m] / (decay_rates[m] - decay_rates[ji]))
-            s_ij *= decay_rates[m] / (decay_rates[m] - decay_rates[j])
-            println(s_ij)
-        end
-    end
-    return s_ij    
-end
+# function transformation_matrix_inverse_ij(i,j,decay_rates)
+#      s_ij = ones(eltype(decay_rates[1]))
+#      s_ij = decay_rates[j]/ (decay_rates[i] - decay_rates[j])
+#     # Loop over generations
+#     for l in 2: (i-j-1)
+#         # Loop over species idnex of species ancestor
+#         for m in j:(i-1)
+#             println(l,m)
+#             println(decay_rates[l])
+#             println(decay_rates[m])
+#             println( (decay_rates[m] - decay_rates[j]))
+#             println(decay_rates[m] / (decay_rates[m] - decay_rates[j]))
+#             s_ij *= decay_rates[m] / (decay_rates[m] - decay_rates[j])
+#             println(s_ij)
+#         end
+#     end
+#     return s_ij    
+# end
 
 
 
