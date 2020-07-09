1416367885c24eb0ef2862d983511a31938d67b3
rannewan <rannewandter@posteo.net>
2020-07-09 17:56:13 +0100

    Fix: Initiliase matrix elements s_ij with type Float64

--- main.jl
+++ main.jl
@@ -7,13 +7,14 @@ end
 
 
 function transformation_matrix_ij(i,j,decay_rates)
-    s_ij = ones(eltype(decay_rates[1]))
+    # s_ij = ones(eltype(decay_rates[1]))
+    s_ij = 1.0
     # Loop over species ancestor index l for species i with (i-j) generations
     for l in j: (i-1)
-        println(l)
-        println(decay_rates[l])
-        println( (decay_rates[l] - decay_rates[i]))
-        println(decay_rates[l] / (decay_rates[l] - decay_rates[i]))
+        # println(l)
+        # println(decay_rates[l])
+        # println( (decay_rates[l] - decay_rates[i]))
+        # println(decay_rates[l] / (decay_rates[l] - decay_rates[i]))
         s_ij *= decay_rates[l] / (decay_rates[l] - decay_rates[i])
     end
     return s_ij
@@ -22,19 +23,19 @@ end
 
 
 function transformation_matrix_inverse_ij(i,j,decay_rates)
-     s_ij = ones(eltype(decay_rates[1]))
+     # s_ij = ones(eltype(decay_rates[1]))
      s_ij = decay_rates[j]/ (decay_rates[i] - decay_rates[j])
-    # Loop over generations
+     # Loop over species ancestor index l for species i with (i-j) generations
     for l in (j+1): (i-1)
-        println(i,j)
-        println(l)
-        println(decay_rates[l])
-        println(decay_rates[j])
-        println( (decay_rates[l] - decay_rates[j]))
-        println(decay_rates[l] / (decay_rates[l] - decay_rates[j]))
+        # println(i,j)
+        # println(l)
+        # println(decay_rates[l])
+        # println(decay_rates[j])
+        # println( (decay_rates[l] - decay_rates[j]))
+        # println(decay_rates[l] / (decay_rates[l] - decay_rates[j]))
         s_ij *= decay_rates[l] / (decay_rates[l] - decay_rates[j])
     end
-    return s_ij    
+    return s_ij
 end
 
 
